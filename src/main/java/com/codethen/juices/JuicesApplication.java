86d5b2e48247c731dcb83470e9d6c8e679a13045
fmaylinch <ferranmaylinch@gmail.com>
2017-04-10 19:56:10 +0200

    Setup dropwizard

---
+++ src/main/java/com/codethen/juices/JuicesApplication.java
@@ -0,0 +1,29 @@
+package com.codethen.juices;
+
+import io.dropwizard.Application;
+import io.dropwizard.assets.AssetsBundle;
+import io.dropwizard.setup.Bootstrap;
+import io.dropwizard.setup.Environment;
+
+public class JuicesApplication extends Application<JuicesConfiguration> {
+
+	public static void main(String[] args) throws Exception {
+        new JuicesApplication().run(args);
+    }
+
+    @Override
+    public String getName() {
+        return "juices";
+    }
+
+    @Override
+    public void initialize(Bootstrap<JuicesConfiguration> bootstrap) {
+        // bootstrap.addBundle(new AssetsBundle("/assets/", "/"));
+    }
+
+    @Override
+    public void run(JuicesConfiguration configuration, Environment environment) {
+
+        // environment.jersey().register(productResource);
+    }
+}
---
+++ src/main/java/com/codethen/juices/JuicesConfiguration.java
@@ -0,0 +1,8 @@
+package com.codethen.juices;
+
+import io.dropwizard.Configuration;
+
+public class JuicesConfiguration extends Configuration {
+
+
+}
