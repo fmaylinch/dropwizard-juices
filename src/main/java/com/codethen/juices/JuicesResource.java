4483d1456ebd4887502ea409b50c4eeb40aea118
fmaylinch <ferranmaylinch@gmail.com>
2017-04-10 20:55:22 +0200

    Create getAll endpoint

--- src/main/java/com/codethen/juices/JuicesResource.java
+++ src/main/java/com/codethen/juices/JuicesResource.java
@@ -5,6 +5,8 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import java.util.ArrayList;
+import java.util.List;
 
 @Path("/juices")
 @Consumes(MediaType.APPLICATION_JSON)
@@ -12,16 +14,38 @@ import javax.ws.rs.core.MediaType;
 
 public class JuicesResource {
 
-	@GET
-	@Path("/fake")
-	public Juice getJuice() {
+	// properties
+	private List<Juice> juices;
+
+
+	// constructor
+	public JuicesResource () {
+
+		juices = new ArrayList<>();
 
 		Juice zumo = new Juice();
-
 		zumo.setName("zumo naranja");
 		zumo.setPrice(3);
 		zumo.setAvailable(true);
+		juices.add(zumo);
 
-		return zumo;
+		Juice zumo2 = new Juice();
+		zumo2.setName("zumo pomelo");
+		zumo2.setPrice(5);
+		zumo2.setAvailable(false);
+		juices.add(zumo2);
+
+		Juice zumo3 = new Juice();
+		zumo3.setName("limonada");
+		zumo3.setPrice(2);
+		zumo3.setAvailable(true);
+		juices.add(zumo3);
+	}
+
+
+	@GET
+	public List<Juice> getAll() {
+
+		return juices;
 	}
 }
